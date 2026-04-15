44c64ca123836708cadaeddb575ff2ed039ed8d0
陳芷羚 <chihling0828@gmail.com>
2026-04-15 23:27:25 +0800

    Update delete.php

--- A1133304＿陳芷羚＿作業3/Example8-7/delete.php
+++ A1133304＿陳芷羚＿作業3/Example8-7/delete.php
@@ -1,1 +1,23 @@
+<?php
+function each(&$array) {
+    $res = array();
+    $key = key($array);
+    if($key !== null){
+        next($array);
+        $res[1] = $res['value'] = $array[$key];
+        $res[0] = $res['key'] = $key;
+    }else{
+        $res = false;
+    }
+    return $res;
+}
 
+$id = $_GET["Id"]; 
+if ( isset($_COOKIE[$id]) ) {  // 檢查Cookie是否存在
+    // 存在，刪除陣列Cookie
+    while ( list($name, $value) = each($_COOKIE[$id]) ) {
+        setcookie($id."[".$name."]", "", time()-3600);
+    }
+}
+header("Location: shoppingcart.php"); 
+?>
