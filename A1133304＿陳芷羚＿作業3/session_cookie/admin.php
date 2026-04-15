b1a5a66f3e19a07e89eccb8cdf7851138cdd4ccc
陳芷羚 <chihling0828@gmail.com>
2026-04-15 23:29:58 +0800

    Update admin.php

--- A1133304＿陳芷羚＿作業3/session_cookie/admin.php
+++ A1133304＿陳芷羚＿作業3/session_cookie/admin.php
@@ -1,1 +1,15 @@
-
+<?php
+session_start();
+if(isset($_SESSION['login'])){
+    if($_SESSION['login']=='admin'){
+        echo"<h1>Welcome!Admin Success</h1>";
+        echo"<a href='logout.php'>Logout</a>";
+    }else{
+        echo"<h1>非法進入網頁</h1>";
+        header("Refresh:2;url=login.php");
+    }
+}else{
+    echo"<h1>非法進入網頁</h1>";
+    header("Refresh:2;url=login.php");
+}
+?>
